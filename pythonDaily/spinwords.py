bdc2f5f8fc953268aa867663cd3a3f1769fd0c9a
mattseatondev <mattseatondev@gmail.com>
2022-11-29 11:42:37 -0500

    first for day

---
+++ pythonDaily/spinwords.py
@@ -0,0 +1,6 @@
+
+sent = 'Hey Hello fellow warriors'
+def spin_words(sentence):
+    return ' '.join([word if len(word) < 5 else word[::-1] for word in sentence.split(' ')])
+
+print(spin_words(sent))
